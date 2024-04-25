3e51d362144eef47181f170efc5af1c75a3b1641
Vasu Dadhaniya <vasu.dadhaniya24@gmail.com>
2024-04-25 20:13:24 +0530

    done1

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ async function main() {
 };
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname,"views"));
+app.set("views", path.join(__dirname,"/views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
